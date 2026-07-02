ae3c694e0b793d7d4d83174b48bf9584b0ddf600
sharmayush11 <ayusharma1127@gmail.com>
2026-07-02 22:16:34 +0530

    js updated

--- script.js
+++ script.js
@@ -24,3 +24,14 @@ const revealObserver = new IntersectionObserver((entries) => {
     });
 }, { threshold: .15 });
 document.querySelectorAll('.reveal').forEach(el => revealObserver.observe(el));
+
+// ---------- Hero typing effect ----------
+const heroLines = [
+    { n: 1, html: '<span class="kw">const</span> developer = {' },
+    { n: 2, html: '&nbsp;&nbsp;name: <span class="str">"Ayush sharma"</span>,' },
+    { n: 3, html: '&nbsp;&nbsp;role: <span class="str">"Frontend Developer"</span>,' },
+    { n: 4, html: '&nbsp;&nbsp;location: <span class="str">"Aligarh, India"</span>,' },
+    { n: 5, html: '&nbsp;&nbsp;stack: [<span class="str">"React"</span>, <span class="str">"JavaScript"</span>, <span class="str">"DSA"</span>, <span class="str">"SQL"</span>],' },
+    { n: 6, html: '&nbsp;&nbsp;status: <span class="str">"open to fresher opportunities"</span> <span class="com"></span>' },
+    { n: 7, html: '};' },
+];
