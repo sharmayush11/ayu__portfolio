94e93cea6b2529aafb82f2b749dd9ca98bdc7602
sharmayush11 <ayusharma1127@gmail.com>
2026-07-03 20:59:39 +0530

    few changes

--- script.js
+++ script.js
@@ -30,10 +30,9 @@ const heroLines = [
     { n: 1, html: '<span class="kw">const</span> developer = {' },
     { n: 2, html: '&nbsp;&nbsp;name: <span class="str">"Ayush sharma"</span>,' },
     { n: 3, html: '&nbsp;&nbsp;role: <span class="str">"Frontend Developer"</span>,' },
-    { n: 4, html: '&nbsp;&nbsp;location: <span class="str">"Aligarh, India"</span>,' },
-    { n: 5, html: '&nbsp;&nbsp;stack: [<span class="str">"React"</span>, <span class="str">"JavaScript"</span>, <span class="str">"DSA"</span>, <span class="str">"SQL"</span>],' },
-    { n: 6, html: '&nbsp;&nbsp;status: <span class="str">"open to fresher opportunities"</span> <span class="com"></span>' },
-    { n: 7, html: '};' },
+    { n: 4, html: '&nbsp;&nbsp;stack: [<span class="str">"React"</span>, <span class="str">"JavaScript"</span>, <span class="str">"DSA"</span>, <span class="str">"SQL"</span>],' },
+    { n: 5, html: '&nbsp;&nbsp;status: <span class="str">"open to fresher opportunities"</span> <span class="com"></span>' },
+    { n: 6, html: '};' },
 ];
 const codeEl = document.getElementById('heroCode');
 const gutterEl = document.getElementById('heroGutter');
